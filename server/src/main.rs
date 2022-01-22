74a65baee64f01d7a6e47493a6cd96a3e5dd5712
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2022-01-22 16:33:01 +0100

    Add logger

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,14 @@
-use actix_web::{HttpServer, App};
+use env_logger::Env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    use actix_web::{HttpServer, App, middleware::Logger};
+    
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
     HttpServer::new(|| {
         App::new()
+            .wrap(Logger::default())
     })
     .bind("127.0.0.1:8080")?
     .run()
