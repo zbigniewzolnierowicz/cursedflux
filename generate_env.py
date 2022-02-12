aef9cd5300ff41618c279625e73b0710d626ee3e
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2022-02-12 16:18:05 +0100

    Add JWT env vars to env gen

--- generate_env.py
+++ generate_env.py
@@ -3,17 +3,30 @@
 from dotenv import dotenv_values
 import os
 
-complete_config = {
+database_config = {
     **dotenv_values("database.env")
 }
 
-complete_config['DATABASE_URL'] = "postgres://{username}:{password}@localhost/{database}".format(
-    username=complete_config['POSTGRES_USER'], password=complete_config['POSTGRES_PASSWORD'],
-    database=complete_config['POSTGRES_DB'])
+jwt_config = {
+    **dotenv_values("jwt.env")
+}
+
+database_config['DATABASE_URL'] = "postgres://{username}:{password}@localhost/{database}".format(
+    username=database_config['POSTGRES_USER'], password=database_config['POSTGRES_PASSWORD'],
+    database=database_config['POSTGRES_DB'])
+
+complete_config = {
+    **database_config,
+    **jwt_config
+}
 
 server_file = open("server/.env", 'w')
 infra_dev_file = open("infra/dev/.env", 'w')
 
-for fd in [server_file, infra_dev_file]:
+for fd in [server_file]:
     for (key, value) in complete_config.items():
+        fd.write("{}=\"{}\"\n".format(key, value))
+
+for fd in [infra_dev_file]:
+    for (key, value) in database_config.items():
         fd.write("{}=\"{}\"\n".format(key, value))
